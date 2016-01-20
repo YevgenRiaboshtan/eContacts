6ba86f22ab9c93d7c6324070cfa2d27ecb6670a8
Yevgen <03.evgeniy.04@gmail.com>
2016-01-20 18:17:05 +0200

    запись логаута
    Запись в базу о логауте при уничтожении сесии.

--- econtactWeb/src/main/java/com/econtact/authWeb/app/beans/view/UserSessionBean.java
+++ econtactWeb/src/main/java/com/econtact/authWeb/app/beans/view/UserSessionBean.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -13,12 +15,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.econtact.authWeb.app.beans.helper.MenuHelper;
 import com.econtact.dataModel.data.context.UserContext;
+import com.econtact.dataModel.data.service.AuthenticationService;
 import com.econtact.dataModel.model.entity.accout.SessionUserEntity;
 
 @Named
 @SessionScoped
 public class UserSessionBean implements Serializable {
 	private static final long serialVersionUID = 5815150040159902787L;
+	
+	@EJB
+	private AuthenticationService authenticationService;
 	
 	private SessionUserEntity principal;
 	private UserContext userContext;
@@ -32,6 +38,11 @@ public class UserSessionBean implements Serializable {
 	public void init() {
 		principal = (SessionUserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		userContext = UserContext.create(getPrincipal(), TimeZone.getTimeZone("GMT+2"));
+	}
+
+	@PreDestroy
+	public void destroy() {
+		authenticationService.disconnectUser("", principal);
 	}
 	
 	public MenuModel getTopMenuModel() {
