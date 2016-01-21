2f2c03ef1492d52f0d36eef78c3b585b2d41e1fb
Yevgen <03.evgeniy.04@gmail.com>
2016-01-21 14:16:44 +0200

    Добавление редактирования учетной записи

--- econtactWeb/src/main/java/com/econtact/authWeb/app/beans/view/UserProfileCRUDBean.java
+++ econtactWeb/src/main/java/com/econtact/authWeb/app/beans/view/UserProfileCRUDBean.java
@@ -7,14 +7,20 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import javax.transaction.UserTransaction;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.econtact.dataModel.data.service.GenericService;
+import com.econtact.dataModel.data.util.UniqueConstraintException;
 import com.econtact.dataModel.model.entity.accout.AccountUserEntity;
+import com.econtact.dataModel.model.entity.accout.SessionUserEntity;
 
 @ManagedBean(name = "userProfileCRUDBean")
 @ViewScoped
 public class UserProfileCRUDBean implements Serializable {
-	private static final long serialVersionUID = -1117175058938629303L;
+	private static final long serialVersionUID = 843495965643883228L;
 
 	@Inject
 	UserSessionBean userSessionBean;
@@ -23,6 +29,9 @@ public class UserProfileCRUDBean implements Serializable {
 	GenericService genericService;
 	
 	private AccountUserEntity entity;
+	
+	private String newFirstName;
+	private String newLastName;
 	
 	@PostConstruct
 	public void init() {
@@ -33,8 +42,34 @@ public class UserProfileCRUDBean implements Serializable {
 		return entity;
 	}
 	
-	public void save() {
-		
+	public void save() throws UniqueConstraintException {
+		if (StringUtils.isNotBlank(newFirstName)) {
+			entity.setFirstName(newFirstName);
+		}
+		if (StringUtils.isNotBlank(newLastName)) {
+			entity.setLastName(newLastName);
+		}
+		entity = genericService.saveOrUpdate(entity, userSessionBean.getUserContext());
+		((SessionUserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setFirstName(entity.getFirstName());
+		((SessionUserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setLastName(entity.getLastName());
+		userSessionBean.init();
+		newFirstName = "";
+		newLastName = "";
 	}
 
+	public String getNewFirstName() {
+		return newFirstName;
+	}
+
+	public void setNewFirstName(String newFirstName) {
+		this.newFirstName = newFirstName;
+	}
+
+	public String getNewLastName() {
+		return newLastName;
+	}
+
+	public void setNewLastName(String newLastName) {
+		this.newLastName = newLastName;
+	}
 }
